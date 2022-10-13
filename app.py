f37cdc7bf2bcbfecb43d5952a6d1256431f887fd
Alam1711 <103541672+Alam1711@users.noreply.github.com>
2022-10-12 23:59:14 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -33,21 +33,29 @@ def checkNumber():
     # Display "Provided input is not an integer!" if value is not a number on result.html page
     global number
     number = request.form['number']
-
+    display = ""
     # Write your to code here to check whether number is even or odd and render result.html page
-
+    if number % 2 == 1:
+        return render_template('result.html', display="Number {Number} is even")
+    elif number % 2 == 0:
+        return render_template('result.html', display="Number {Number} is odd")
+    elif number == None:
+        return render_template('result.html', display="No number provided")
+    else: 
+        return render_template('result.html', display="Provided input is not an integer!")
 
 @app.get('/addStudentOrganisation')
 def displayStudentForm():
     # Complete this function to display studentFrom.html page
-    pass
+    return render_template('studentDetails.html')
 
 
 @app.route('/addStudentOrganisation', methods=['POST'])
 def displayRegistrationPage():
     # Get student name and organisation from form.
     studentName = request.form['name']
-
+    organizationName = request.form['orgName']
     # Append this value to studentOrganisationDetails
-
+    combined = studentName + organizationName
     # Display studentDetails.html with all students and organisations
+    return render_template('studentDetails.html', combined = combined)
